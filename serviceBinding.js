b9d470ebf94fc029187972bbf54fa0bded75d9bc
molzi <maximilian.olzinger@ksic.at>
2020-06-22 08:31:37 +0000

    Testing-Files deleted

--- serviceBinding.js
+++ serviceBinding.js
@@ -1,5 +1,1 @@
-function initModel() {
-	var sUrl = "/sap/opu/odata/sap/ZHOFIO_CUSTOMER_SRV/";
-	var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
-	sap.ui.getCore().setModel(oModel);
-}
+function initModel() {}
